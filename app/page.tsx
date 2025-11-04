668ba431ec1c50422a527d802dd5771054c00bb3
Deniz <denizaskar05@gmail.com>
2025-11-04 18:37:00 +0300

    Update homepage layout, add CompanionCard and CompanionList components, and create table structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
-import { Button } from "@/components/ui/button"
+import CompanionCard from "@/components/CompanionCard"
+import CompanionsList from "@/components/CompanionsList"
+import CTA from "@/components/CTA"
+import { recentSessions } from "@/constants"
 
 const Page = () => {
   return (
-    <div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+      <CompanionCard
+      id="123"
+      name="Neura the Brainy Explorer"
+      topic="Neural Network of the Brain"
+      subject="science"
+      duration={45}
+      color="#ffda6e"
+      />
+       <CompanionCard
+      id="456"
+      name="Countsy the Number Wizard"
+      topic="Derivatives and Integrals"
+      subject="Maths"
+      duration={30}
+      color="#e5d0ff"
+      />
+       <CompanionCard
+      id="789"
+      name="Verba the Vocabulary Builder"
+      topic="language"
+      subject="English Literature"
+      duration={30}
+      color="#BDEE7FF"
+      />
       
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>
-        Let's Get Started
-      </Button>
-    </div>
+      </section>
+      <section className="home-section">
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        className="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+
+      </section>
+
+    </main>
   )
 }
 
