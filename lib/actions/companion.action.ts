7e265f286616cb8adf9bec1f77ede20f2e6ec9cc
Deniz <denizaskar05@gmail.com>
2025-11-09 23:14:15 +0300

    feat: integrate Vapi SDK and update companion details page logic

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -2,6 +2,7 @@
 
 import {auth} from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase"
+import { create } from "domain";
 
 export const createCompanion = async (formData: CreateCompanion) =>{
 
@@ -40,4 +41,16 @@ export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }:
     if(error) throw new Error(error.message);
 
     return companions;
+}
+
+export const getCompanion  = async (id: string) =>{
+    const supabase =createSupabaseClient();
+
+    const {data,error} = await supabase
+    .from('companions')
+    .select()
+    .eq('id',id)
+
+    if(error) return console.log(error)
+    return data[0]
 }
